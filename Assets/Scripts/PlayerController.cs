feacf2f8b74aacb93b92ac0f28d5bc69430892b8
frederik desmet <info@neo-level.com>
2021-01-16 13:25:59 +0100

    Added jump mechanism to the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float _speed = 20.0f;
+    private const float Speed = 20.0f;
+    private const float JumpForce = 20.0f;
     private float _horizontalInput;
 
+    private Rigidbody _playerRigidbody;
+    
+    
+    
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        // Gets the rigid body component from the player object.
+        _playerRigidbody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     private void Update()
     {
         GetUserInput();
+        UserJump();
     }
 
     // Retrieve the user input when pressing the arrow buttons.
@@ -25,6 +35,17 @@ public class PlayerController : MonoBehaviour
         _horizontalInput = Input.GetAxis("Horizontal");
 
         // Move the user based on the Horizontal input.
-        transform.Translate(Vector3.right * (Time.deltaTime * _speed * _horizontalInput));
+        transform.Translate(Vector3.right * (Time.deltaTime * Speed * _horizontalInput));
+    }
+    
+    private void UserJump ()
+    {
+        // Checks if the player presses the space bar.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Adds physics to the player allowing an upwards jump.
+            // Immediately applies force. using the impulse mode.
+            _playerRigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
     }
 }
