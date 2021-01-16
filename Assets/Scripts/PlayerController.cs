1bd430468d123d8eb600bc79c1c5385988e1ff7c
frederik desmet <info@neo-level.com>
2021-01-16 13:18:49 +0100

    addition of player movement on horizontal axis.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float _speed = 20.0f;
+    private float _horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        GetUserInput();
+    }
+
+    // Retrieve the user input when pressing the arrow buttons.
+    private void GetUserInput()
+    {
+        // Get the user input for horizontal movement.
+        _horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the user based on the Horizontal input.
+        transform.Translate(Vector3.right * (Time.deltaTime * _speed * _horizontalInput));
     }
 }
