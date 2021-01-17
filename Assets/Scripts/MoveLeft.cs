691331fd777e029c1b22e09de0d6e85b9362a7ec
frederik desmet <info@neo-level.com>
2021-01-17 17:56:56 +0100

    Created a move left script that make the enemies move left on the screen.

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private float xDestroy = -10f;
+    
+    private Rigidbody objectRigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectRigidbody.AddForce(Vector3.left * speed);
+        if (transform.position.x < xDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
